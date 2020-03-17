92eeb650225ac654d7f3e59c94e28da024c0e7b4
Ciro da Silva Lemos <N0550407@bcnet.bcb.gov.br>
2020-03-17 17:57:34 -0300

    Carga inicial

---
+++ src/app/products/product-detail.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Product, ProductResolved } from './product';
+import { ProductService } from './product.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit{
+  pageTitle = 'Product Detail';
+  product: Product;
+  errorMessage: string;
+
+  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+
+    //Para rotas normais.
+    //const id = +this.route.snapshot.paramMap.get('id');
+    //this.getProduct(id);
+
+    //para Prefetching Data (Buscando conteúdo antes de mostrar o componente através da rota solicitada.)
+    const resolvedData: ProductResolved = this.route.snapshot.data['resolvedData'];
+    this.errorMessage = resolvedData.error;
+    this.onProductRetrieved(resolvedData.product);
+
+  }
+
+  //Precisamos de metodo apenas quando não estamos usando Prefetching Data route.
+  getProduct(id: number) {
+    this.productService.getProduct(id).subscribe({
+      next: product => this.onProductRetrieved(product),
+      error: err => this.errorMessage = err
+    });
+  }
+
+  onProductRetrieved(product: Product): void {
+    this.product = product;
+
+    if (this.product) {
+      this.pageTitle = `Product Detail: ${this.product.productName}`;
+    } else {
+      this.pageTitle = 'No product found';
+    }
+  }
+}
