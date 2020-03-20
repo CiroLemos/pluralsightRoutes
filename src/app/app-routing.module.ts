5929b25f2064466b705cdcae0ca552a5b5ffb123
Ciro da Silva Lemos <N0550407@bcnet.bcb.gov.br>
2020-03-20 15:35:09 -0300

    Curso concluido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
+import { AuthGuard } from './user/auth.guard';
+import { SelectiveStrategy } from './selective-stratety.service';
 
 const ROUTES = [
     { path: 'welcome', component: WelcomeComponent },
+    { path: 'products', 
+        canActivate: [AuthGuard],
+        data: { preload: true },
+        loadChildren: () => 
+            import("./products/product.module").then(m => m.ProductModule)
+    },
     { path: '', redirectTo: 'welcome', pathMatch: 'full' },
     { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(ROUTES, { enableTracing: true })
+        RouterModule.forRoot(ROUTES, { enableTracing: true, preloadingStrategy: SelectiveStrategy })
     ],
     exports: [RouterModule]
 })
--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -12,37 +12,64 @@ import { ProductEditTagsComponent } from './product-edit/product-edit-tags.compo
 import { AuthGuard } from '../user/auth.guard';
 import { ProductEditGuard } from './product-edit/product-edit.guard';
 
+//Configuração da rota quando o projeto não estava configurado para lazy load.
+const ROUTE_NO_LAZY =
+  [
+    {
+      path: 'products',
+      canActivate: [AuthGuard],
+      children: [
+        {
+          path: '',
+          component: ProductListComponent
+        },
+        {
+          path: ':id',
+          component: ProductDetailComponent,
+          resolve: { resolvedData: ProductResolverService }
+        },
+        {
+          path: ':id/edit', component: ProductEditComponent,
+          canDeactivate: [ProductEditGuard],
+          resolve: { resolvedData: ProductResolverService },
+          children: [
+            { path: '', redirectTo: 'info', pathMatch: 'full' },
+            { path: 'info', component: ProductEditInfoComponent },
+            { path: 'tags', component: ProductEditTagsComponent }
+          ]
+        }
+      ]
+    }
+  ];
+
+  //Configuração para lazy loading.
+  const ROUTE_WITH_LAZY = 
+  [
+    {
+      path: '',
+      component: ProductListComponent
+    },
+    {
+      path: ':id',
+      component: ProductDetailComponent,
+      resolve: { resolvedData: ProductResolverService }
+    },
+    {
+      path: ':id/edit', component: ProductEditComponent,
+      canDeactivate: [ProductEditGuard],
+      resolve: { resolvedData: ProductResolverService },
+      children: [
+        { path: '', redirectTo: 'info', pathMatch: 'full' },
+        { path: 'info', component: ProductEditInfoComponent },
+        { path: 'tags', component: ProductEditTagsComponent }
+      ]
+    }
+  ]
+
 @NgModule({
   imports: [
     SharedModule,
-    RouterModule.forChild([
-      {
-        path: 'products',
-        canActivate: [AuthGuard],
-        children: [
-          {
-            path: '', 
-            component: ProductListComponent
-          },
-          {
-            path: ':id',
-            component: ProductDetailComponent,
-            resolve: { resolvedData: ProductResolverService }
-          },
-          {
-            path: ':id/edit', component: ProductEditComponent,
-            canDeactivate: [ProductEditGuard],
-            resolve: { resolvedData: ProductResolverService },
-            children: [
-              { path: '', redirectTo: 'info', pathMatch: 'full' },
-              { path: 'info', component: ProductEditInfoComponent },
-              { path: 'tags', component: ProductEditTagsComponent }
-            ]
-          }
-        ]
-      },
-
-    ])
+    RouterModule.forChild(ROUTE_WITH_LAZY)
   ],
   declarations: [
     ProductListComponent,
